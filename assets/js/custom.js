6bd560687671e3d18350ac62baeefd05e39b7fee
Mariusz Nowak <medyk@medikoo.com>
2016-10-20 13:00:19 +0200

    Remove dubious log

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,4 @@
 $(document).ready(function(e) {
-	console.log(location);
-
 	if(location.href.indexOf("tanzania") !== -1) {
 		$('.logo-default').css('display', 'none');
 		$('.logo-tanzania').css('display', 'block');
