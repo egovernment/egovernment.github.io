2022f4de1d8820a3cc106f8425d248c15c0b8d97
melux <meluxford@meluxford.com>
2017-01-13 16:03:44 +0100

    test with menu and page layout

--- _analysis/elsalvador.markdown
+++ _analysis/elsalvador.markdown
@@ -6,6 +6,7 @@ lang: en
 category: analysis
 ref: analysis-els
 weight: 1
+userOnly: true
 ---
 
 ![MiEmpresa.gob.sv](/img/logo-bg-white-v.png)  
@@ -13,7 +14,7 @@ weight: 1
 
 
 Name of service | Mockups | Analysis BPA | Form | In charge
---- | --- | --- | --- |--- 
+--- | --- | --- | --- |---
 Capital registration of foreign branch | [link](https://dadd7a.axshare.com/#p=guide){:target="_blank"} | [link](https://docs.google.com/document/d/10H-GeCcE591dXQTuzyScWl44dwNZC6s07tyqGWbCRtg/edit?usp=sharing){:target="_blank"} | [link](https://docs.google.com/spreadsheets/d/15IU0AD9JGx-qZKIO1_uhfvGuxl7_Aqdl_h8xUYNAt3Y/edit?usp=sharing){:target="_blank"} | Gualberto
 SME registration | [link](https://w14glk.axshare.com/#p=guide){:target="_blank"} | [link](https://docs.google.com/document/d/1-B-huszzTx5kesEPx0w5pTLDbZuVeixHrbkhek7lefU/edit?usp=sharing){:target="_blank"} | [link](https://docs.google.com/spreadsheets/d/1vlYF-g_iXo7kZcfZgcpjMtmTTbONaB-G3OH-rJrOrcE/edit?usp=sharing){:target="_blank"} | Ernesto
 Company operations | [link](){:target="_blank"} | [link](https://docs.google.com/document/d/15XdsHUuMTINYNJ_TovomwmNewUJ38Sngvuxy-bVj7ZA/edit?usp=sharing){:target="_blank"} | [link](https://drive.google.com/open?id=1uJaP5c6dGjUXSnIWM3AWYVBMRg8LvtvWChYSL0cLm0A){:target="_blank"} |
---
+++ assets/js/auth.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function userConnexion() {
+	this.checkSetup();
+
+	this.emailInput = "guest@eregistrations.org";
+	//this.passwordInput = document.getElementById('login-password-input');
+	this.passwordInput = "";
+
+	//this.signInmodalButton = document.getElementById('login-btn-modal');
+	this.signInButton = document.getElementById('login-modal-button');
+	this.signOutButton = document.getElementById('btn-logout');
+
+	this.signInButton.addEventListener('click', this.signIn.bind(this));
+	this.signOutButton.addEventListener('click', this.signOut.bind(this));
+	//this.signInmodalButton.addEventListener('click', this.openSignInForm.bind(this));
+
+	this.initFirebase();
+}
+
+userConnexion.prototype.initFirebase = function() {
+	$(".user-waiter").removeClass('hidden');
+	$(".user-only").addClass('hidden');
+	this.auth = firebase.auth();
+	this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+}
+
+userConnexion.prototype.signIn = function() {
+	//this.auth.signInWithPopup(provider);
+	console.log();
+	this.passwordInput = $('#login-password-input').val();
+	this.userInput = $('#login-user-input').val();
+	if(this.passwordInput != '' && this.userInput == "guest") {
+		this.auth.signInWithEmailAndPassword(this.emailInput, this.passwordInput);
+	}
+}
+
+userConnexion.prototype.signOut = function() {
+	this.auth.signOut();
+	$("#btn-logout").toggleClass('hidden');
+	$("#btn-login").toggleClass('hidden');
+	$(".user-waiter").removeClass('hidden');
+	$(".user-only").addClass('hidden');
+
+}
+
+userConnexion.prototype.onAuthStateChanged = function(user) {
+	$(".user-waiter").removeClass('hidden');
+	$(".user-only").addClass('hidden');
+	console.log(user);
+	if(user) {
+		console.log("user logged");
+		$("#btn-logout").toggleClass('hidden');
+		$("#btn-login").toggleClass('hidden');
+		$(".user-waiter").addClass('hidden');
+		$(".user-only").removeClass('hidden');
+
+		/*if(user.displayName != undefined && user.displayName != "undefined") {
+			this.userName.textContent = user.displayName;
+		}else{
+			this.userName.textContent = "User logged";
+		}*/
+		$('#login-modal').modal('hide');
+	}else{
+		console.log("user not logged");
+		$(".user-only").addClass('hidden');
+		$(".user-waiter").removeClass('hidden');
+	}
+}
+
+userConnexion.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions.');
+  } else if (config.storageBucket === '') {
+    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+        'actually a Firebase bug that occurs rarely. ' +
+        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+        'and make sure the storageBucket attribute is not empty. ' +
+        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
+        'displayed there.');
+  }
+};
+
+window.onload = function() {
+  window.userConnexion = new userConnexion();
+};
